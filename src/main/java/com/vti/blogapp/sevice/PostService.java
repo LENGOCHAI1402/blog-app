a6a47d9ad20547613f7c3e641891b365a437642b
Your Name <lengochai04022002@gmail.com>
2023-12-07 18:18:43 +0700

    [Add] Find all post API

--- src/main/java/com/vti/blogapp/sevice/PostService.java
+++ src/main/java/com/vti/blogapp/sevice/PostService.java
@@ -2,8 +2,13 @@ package com.vti.blogapp.sevice;
 
 import com.vti.blogapp.dto.PostDto;
 import com.vti.blogapp.form.PostCreateForm;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 
 public interface PostService {
+    Page<PostDto> findAll(Pageable pageable);
+
     PostDto create(PostCreateForm form);
 
 }
