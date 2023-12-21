a6d8eea6d4585a8eee285064f06e3e163d0bfa13
Your Name <lengochai04022002@gmail.com>
2023-12-21 19:57:25 +0700

    [Add] Login using database and Handle 41 status

--- src/main/java/com/vti/blogapp/sevice/UserServiceImpl.java
+++ src/main/java/com/vti/blogapp/sevice/UserServiceImpl.java
@@ -5,12 +5,17 @@ import com.vti.blogapp.form.UserCreateForm;
 import com.vti.blogapp.mapper.UserMapper;
 import com.vti.blogapp.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
 
@@ -22,4 +27,14 @@ public class UserServiceImpl implements UserService {
         var saveUser = repository.save(user);
         return UserMapper.map(saveUser);
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        var user = repository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+        return new User(user.getUsername(), user.getPassword(), AuthorityUtils.NO_AUTHORITIES);
+    };
 }
